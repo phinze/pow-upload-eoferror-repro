fedfda773a308ea5f508c59f8fba5fb801c38f3b
phinze <paul.t.hinze@gmail.com>
2013-05-23 10:58:14 -0500

    rails new pow-upload-eoferror-repro -T -J -S

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PowUploadEoferrorRepro::Application.config.secret_key_base = '105f0e4a374fb5a97e2a26e92dcfb9e9e897c0bf92b5a2cc075dcffdb5d4104d4ddb3e4d87764c634492cae62dc7eabe4a9af5048b52bfec5345da492d36dec6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PowUploadEoferrorRepro::Application.config.session_store :cookie_store, key: '_pow-upload-eoferror-repro_session'
